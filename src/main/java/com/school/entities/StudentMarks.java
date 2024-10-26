a16c9cafbeeaf5ff42b14f779293a67f98d91f82
Nernika Boya Raju <88661224+nenrikaraju123@users.noreply.github.com>
2024-10-26 15:14:56 +0530

    Update StudentMarks.java

--- src/main/java/com/school/entities/StudentMarks.java
+++ src/main/java/com/school/entities/StudentMarks.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 public class StudentMarks {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int id;
 	int regNo;
 	String studentName;
